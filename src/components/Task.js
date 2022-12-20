84cca933a3023f0adf2191c42c0578d736db4fcd
Uche Audrey <uaudrey3@gmail.com>
2022-12-20 07:28:02 -0800

    updated toggle

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 import './Task.css';
 
 const Task = ({ id, title, isComplete }) => {
+  const [completed, setCompleted] = useState(isComplete);
+
+  const toggleComplete = () => {
+    setCompleted(!completed);
+  };
+
+  const toggleClass = completed
+    ? 'tasks__item__toggle'
+    : 'tasks__item__toggle--completed';
+
   return (
     <li className="tasks__item">
-      <button className="tasks__item__toggle">{title}</button>
+      <button onClick={toggleComplete} className={toggleClass}>
+        {title}
+      </button>
       <button className="tasks__item__remove button">x</button>
     </li>
   );
